6ab5bf419e40f1582679d1ef129c7fe6857663ac
Mariia Beliakova <mari.mv2008@gmail.com>
2024-10-16 17:01:48 +0200

    mobile адаптивность блоков на главной странице

--- build-tools/src/components/js/our-team.js
+++ build-tools/src/components/js/our-team.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     var swiper_block_rour_team = new Swiper('.our-team-swiper', {
         direction: 'horizontal',
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 40,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 10,
         loop: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 20,
+            },
+            1200: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 15,
+            },
+        },
         navigation: {
             nextEl: '.our-team__swiper-button-next',
             prevEl: '.our-team__swiper-button-prev',
--- build-tools/src/components/js/reviews.js
+++ build-tools/src/components/js/reviews.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     var swiper_block_reviews = new Swiper('.reviews-swiper', {
         direction: 'horizontal',
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 40,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 10,
         loop: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 20,
+            },
+            1200: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 40,
+            },
+        },
         navigation: {
             nextEl: '.reviews__swiper-button-next',
             prevEl: '.reviews__swiper-button-prev',
