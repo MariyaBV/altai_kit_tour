621e3b806c5ba18df731e115bae2aa55ba24750e
Mariia Beliakova <mari.mv2008@gmail.com>
2024-10-22 23:07:09 +0200

    мобильное меню

--- build-tools/src/components/js/menu.js
+++ build-tools/src/components/js/menu.js
@@ -1,6 +1,11 @@
 $(document).ready(function ($) {
     // проверка URL и отображение каталога
     function checkCatalogInUrl() {
+        // Проверяем ширину экрана перед выполнением функции
+        if ($(window).width() < 767) {
+            return; // Прерываем выполнение, если ширина экрана меньше 768px
+        }
+
         var url = window.location.href;
         var blockCatalog = $('#block-tours');
         var header = $('#main-header');
@@ -19,6 +24,12 @@ $(document).ready(function ($) {
     // открытие/закрытие каталога
     function toggleCatalog(event) {
         event.preventDefault();
+
+        // Проверяем ширину экрана перед выполнением функции
+        if ($(window).width() < 767) {
+            return; // Прерываем выполнение, если ширина экрана меньше 768px
+        }
+
         var blockCatalog = $('#block-tours');
         var header = $('#main-header');
         var url = window.location.href;
@@ -62,11 +73,11 @@ $(document).ready(function ($) {
     // сброс списка - скрываются все элементы, кроме первого
     function resetToFirstItem() {
         var firstParent = $('.block-tours__list_item').first();
-        
+
         // Скрываем все родительские элементы и подменю
         $('.block-tours__list_item').removeClass('visible').addClass('hidden');
         $('.block-tours__list_item__inner').removeClass('visible').addClass('hidden');
-        
+
         // Оставляем первый родительский элемент видимым
         firstParent.addClass('visible').removeClass('hidden');
         var submenu = firstParent.find('.block-tours__list_item__inner');
@@ -78,6 +89,12 @@ $(document).ready(function ($) {
     // клик по родительским элементам
     function handleParentClick(event) {
         event.preventDefault();
+
+        // Проверяем ширину экрана перед выполнением функции
+        if ($(window).width() < 767) {
+            return; // Прерываем выполнение, если ширина экрана меньше 768px
+        }
+
         var parentItem = $(this).parent();
 
         // Закрываем все списки и удаляем класс visible у всех родителей
@@ -100,10 +117,92 @@ $(document).ready(function ($) {
 
     // родительские элементы
     $('.block-tours__list_item > a').on('click', handleParentClick);
+});
 
+$(document).ready(function () {
     // меню бургер
     $('#burger-menu').click(function () {
         $(this).toggleClass('active');
-        $('#site-navigation').toggleClass('visible');
+        $('.header .top-menu').toggleClass('visible');
+        $('#overlay').toggleClass('visible');
     });
 });
+
+//выпадающий список
+$(document).ready(function () {
+    // Открытие и закрытие подменю при клике на любой элемент с подменю
+    $('#site-navigation').on('click', '.menu-item-has-children > a', function (event) {
+        event.preventDefault();
+
+        // Проверяем ширину экрана перед выполнением функции
+        if ($(window).width() > 768) {
+            return; // Прерываем выполнение, если ширина экрана больше 768px
+        }
+
+        var $menu = $(this).closest('.menu-item-has-children').find('ul.sub-menu').first();
+        var parentText = $(this).find('p').text().trim(); // Текст текущего пункта меню
+
+        // Проверяем состояние меню (открытое/закрытое)
+        if ($menu.hasClass('show')) {
+            // Закрываем текущее меню
+            $menu.removeClass('show').addClass('hide');
+            $(this).removeClass('selected-item-menu');
+
+            // Если все подменю закрыты, скрываем кнопку "Назад"
+            if ($('#site-navigation .sub-menu.show').length === 0) {
+                $('#menu-back-button').addClass('hidden');
+            }
+        } else {
+            // Закрываем все открытые подменю
+            $('#site-navigation .sub-menu').removeClass('show').removeClass('hide');
+            $('#site-navigation a.selected-item-menu').removeClass('selected-item-menu');
+
+            // Открываем текущее подменю и добавляем активный класс
+            $menu.removeClass('hide').addClass('show'); // Открываем текущее меню
+            $(this).addClass('selected-item-menu');
+
+            // Показать кнопку "Назад" с текстом текущего меню
+            $('#menu-back-button').removeClass('hidden');
+            $('#menu-back-button .back-text').text(parentText);
+        }
+
+        // Проверяем состояние родит элемента
+        var $parentMenu = $(this).closest('.menu-item-has-children').parent().closest('ul.sub-menu');
+
+        if ($menu.hasClass('show') && $parentMenu) {
+            $parentMenu.addClass('hide');
+            $parentMenu.removeClass('show');
+        } else if ($menu.hasClass('hide')) {
+            $parentMenu.addClass('show');
+            $parentMenu.removeClass('hide');
+            
+
+        }
+    });
+
+    // Обработка кнопки "Назад"
+    $('#menu-back-button').on('click', 'a.back-button', function (e) {
+        e.preventDefault();
+
+        // Закрываем последнее открытое подменю и возвращаемся на уровень выше
+        var $lastOpenedMenu = $('#site-navigation .sub-menu.show').last();
+        $lastOpenedMenu.removeClass('show');
+        $parentOpenedPrev = $lastOpenedMenu.closest('.menu-item-has-children').parent().closest('ul.sub-menu');
+        $parentOpenedPrev.removeClass('hide').addClass('show');
+        var parentOpenedPrevPrevText = $parentOpenedPrev.closest('.menu-item-has-children').parent().find('p').first().text().trim();
+        $('#menu-back-button .back-text').text(parentOpenedPrevPrevText);
+        $parentOpenedPrev.siblings('a.menu-list__link').addClass('selected-item-menu');
+        var $lastSelectedItem = $('#site-navigation a.selected-item-menu').last();
+        $lastSelectedItem.removeClass('selected-item-menu');
+
+        // Если подменю больше нет, скрываем кнопку "Назад"
+        if ($('#site-navigation .sub-menu.show').length === 0) {
+            $('#menu-back-button').addClass('hidden');
+        } else {
+            // Меняем текст кнопки "Назад" на предыдущий уровень меню
+            var newBackText = $('#site-navigation .menu-item-has-children > a.selected-item-menu').last().find('p').text().trim();
+            $('#menu-back-button .back-text').text(newBackText);
+        }
+    });
+});
+
